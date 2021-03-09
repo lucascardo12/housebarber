f8ff4d3a6a9e169b61cc82789250dd3e39eca99b
Hugo Alves <guinhoalves.cba@gmail.com>
2021-03-09 09:14:31 -0400

    Melhorias na Listagem de Produtos

--- lib/pages/cadastros/lista-clientes-page.dart
+++ lib/pages/cadastros/lista-clientes-page.dart
@@ -9,8 +9,6 @@ class ListaClientes extends StatefulWidget {
 }
 
 class _ListaClientestState extends State<ListaClientes> {
-
-
   List<Cliente> listadeCliente = <Cliente>[];
   @override
   void initState() {
@@ -29,13 +27,10 @@ class _ListaClientestState extends State<ListaClientes> {
           title: Text('Clientes'),
         ),
         body: ListView.builder(
-          padding: EdgeInsets.only(top: 15),
+          padding: EdgeInsets.only(top: 20),
           itemCount: listadeCliente.length,
           itemBuilder: (context, index) {
             return _buildItem(context, listadeCliente[index]);
-            // return ListTile(
-            //   subtitle: Text(listadeCliente[index].email),
-            //   title: Text(listadeCliente[index].nome),
           },
         ),
         floatingActionButton: FloatingActionButton.extended(
@@ -45,210 +40,216 @@ class _ListaClientestState extends State<ListaClientes> {
           label: Text('Cliente'),
         ));
   }
-}
 
-_buildItem(BuildContext context, Cliente cliente) {
-  return Container(
-    padding: EdgeInsets.only(left: 15, right: 15, bottom: 15),
-    child: Card(
-      elevation: 4,
-      child: Material(
-        child: InkWell(
-          onTap: () {
-            showSimpleCustomDialog(context, cliente);
-          },
-          child: Container(
-            padding: EdgeInsets.all(10),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  cliente.nome,
-                  style: TextStyle(
-                    fontFamily: "Myriad Pro",
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: -.5,
+  _buildItem(BuildContext context, Cliente cliente) {
+    return Container(
+      padding: EdgeInsets.only(left: 15, right: 15, bottom: 15),
+      child: Card(
+        elevation: 4,
+        child: Material(
+          child: InkWell(
+            onTap: () {
+              setState(() {
+                showSimpleCustomDialog(context, cliente);
+              });
+            },
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    cliente.nome,
+                    style: TextStyle(
+                      fontFamily: "Myriad Pro",
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: -.5,
+                    ),
                   ),
-                ),
-                SizedBox(height: 5),
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    Row(
-                      children: [
-                        Container(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                "Email:",
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w600,
+                  SizedBox(height: 5),
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Row(
+                        children: [
+                          Container(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  "Email:",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600,
+                                  ),
                                 ),
-                              ),
-                              Text(
-                                cliente.email,
-                                style: TextStyle(
-                                  fontSize: 16,
+                                Text(
+                                  cliente.email,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                  ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
+                        ],
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 8),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "WhatsApp:",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
                         ),
-                      ],
-                    )
-                  ],
-                ),
-                SizedBox(height: 8),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      "WhatsApp:",
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600,
                       ),
-                    ),
-                    Text(
-                      cliente.numero ?? "",
-                      style: TextStyle(
-                        fontSize: 16,
+                      Text(
+                        cliente.numero ?? "",
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                        overflow: TextOverflow.ellipsis,
                       ),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
       ),
-    ),
-  );
-}
+    );
+  }
 
-void showSimpleCustomDialog(BuildContext context, Cliente cliente) {
-  TextEditingController nome = TextEditingController(text: cliente.nome);
-  TextEditingController cpf = TextEditingController(text: cliente.cpf);
-  TextEditingController email = TextEditingController(text: cliente.email);
-  TextEditingController numero = TextEditingController(text: cliente.numero);
-  AlertDialog simpleDialog = AlertDialog(
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(12.0),
-    ),
-    content: Container(
-      padding: EdgeInsets.all(5),
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+  void showSimpleCustomDialog(BuildContext context, Cliente cliente) {
+    TextEditingController nome = TextEditingController(text: cliente.nome);
+    TextEditingController cpf = TextEditingController(text: cliente.cpf);
+    TextEditingController email = TextEditingController(text: cliente.email);
+    TextEditingController numero = TextEditingController(text: cliente.numero);
+    AlertDialog simpleDialog = AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      content: Container(
+        padding: EdgeInsets.all(5),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                controller: nome,
+                decoration: InputDecoration(
+                  isDense: false,
+                  labelText: "Nome:",
+                ),
+                onChanged: (v) {},
+              ),
+              SizedBox(height: 5),
+              TextFormField(
+                controller: cpf,
+                decoration: InputDecoration(
+                  isDense: false,
+                  labelText: "CPF:",
+                ),
+                onChanged: (v) {},
+              ),
+              SizedBox(height: 5),
+              TextFormField(
+                controller: email,
+                decoration: InputDecoration(
+                  isDense: false,
+                  labelText: "Email:",
+                ),
+                onChanged: (v) {},
+              ),
+              SizedBox(height: 5),
+              TextFormField(
+                controller: numero,
+                decoration: InputDecoration(
+                  isDense: false,
+                  labelText: "WhatsApp:",
+                ),
+                onChanged: (v) {},
+              ),
+              SizedBox(height: 5),
+            ],
+          ),
+        ),
+      ),
+      actions: <Widget>[
+        Row(
           children: <Widget>[
-            TextFormField(
-              controller: nome,
-              decoration: InputDecoration(
-                isDense: false,
-                labelText: "Nome:",
+            RaisedButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(5),
               ),
-              onChanged: (v) {},
-            ),
-            SizedBox(height: 5),
-            TextFormField(
-              controller: cpf,
-              decoration: InputDecoration(
-                isDense: false,
-                labelText: "CPF:",
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text(
+                'CANCELAR',
+                style: TextStyle(fontSize: 12),
               ),
-              onChanged: (v) {},
             ),
-            SizedBox(height: 5),
-            TextFormField(
-              controller: email,
-              decoration: InputDecoration(
-                isDense: false,
-                labelText: "Email:",
+            SizedBox(width: 5),
+            FlatButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(5),
               ),
-              onChanged: (v) {},
-            ),
-            SizedBox(height: 5),
-            TextFormField(
-              controller: numero,
-              decoration: InputDecoration(
-                isDense: false,
-                labelText: "WhatsApp:",
+              color: Colors.green,
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    'SALVAR',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                ],
               ),
-              onChanged: (v) {},
+              onPressed: () {
+                Map<String, dynamic> updateClinente = {
+                  'nome': nome.text,
+                  'idUser': cliente.idUser,
+                  'cpf': cpf.text,
+                  'email': email.text,
+                  'numero': numero.text,
+                  'id': cliente.id,
+                };
+                _updateCadastroCliente(updateClinente);
+                setState(() {
+                  Navigator.of(context).pop();
+                });
+                //Navigator.pushNamed(context, '/listaClientes');
+              },
             ),
-            SizedBox(height: 5),
+            SizedBox(
+              width: 20,
+            ),
           ],
         ),
-      ),
-    ),
-    actions: <Widget>[
-      Row(
-        children: <Widget>[
-          RaisedButton(
-            shape: RoundedRectangleBorder(
-              borderRadius: new BorderRadius.circular(5),
-            ),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: Text(
-              'CANCELAR',
-              style: TextStyle(fontSize: 12),
-            ),
-          ),
-          SizedBox(width: 5),
-          FlatButton(
-            shape: RoundedRectangleBorder(
-              borderRadius: new BorderRadius.circular(5),
-            ),
-            color: Colors.green,
-            child: Row(
-              children: <Widget>[
-                Text(
-                  'SALVAR',
-                  style: TextStyle(fontSize: 12),
-                ),
-              ],
-            ),
-            onPressed: () {
-              Map<String, dynamic> updateClinente = {
-                'nome': nome.text,
-                'idUser': cliente.idUser,
-                'cpf': cpf.text,
-                'email': email.text,
-                'numero': numero.text,
-                'id': cliente.id,
-              };
-              _updateCadastroCliente(updateClinente);
-              //Navigator.of(context).pop();
-              Navigator.pushNamed(context, '/listaClientes');
-            },
-          ),
-          SizedBox(
-            width: 20,
-          ),
-        ],
-      ),
-    ],
-  );
-  showDialog(context: context, builder: (BuildContext context) => simpleDialog);
-}
+      ],
+    );
+    showDialog(
+        context: context, builder: (BuildContext context) => simpleDialog);
+  }
 
-void _updateCadastroCliente(updateClinente) {
-  // print(cliente.nome);
-
-  var updateCliente = new Cliente(
-      nome: updateClinente['nome'],
-      cpf: updateClinente['cpf'],
-      email: updateClinente['email'],
-      numero: updateClinente['numero'],
-      id: updateClinente['id'],
-      idUser: updateClinente['idUser']);
-  bacon.insertUpdate(objeto: updateCliente, tabela: 'Cliente');
+  void _updateCadastroCliente(updateCliente) {
+    // print(cliente.nome);
+    setState(() {
+      var cliente = new Cliente(
+          nome: updateCliente['nome'],
+          cpf: updateCliente['cpf'],
+          email: updateCliente['email'],
+          numero: updateCliente['numero'],
+          id: updateCliente['id'],
+          idUser: updateCliente['idUser']);
+      bacon.insertUpdate(objeto: cliente, tabela: 'Cliente');
+    });
+  }
 }
--- lib/pages/cadastros/lista-servico-page.dart
+++ lib/pages/cadastros/lista-servico-page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:housebarber/config/global.dart';
 import 'package:housebarber/model/produtoServico.dart';
 
 class ListaProdutoServico extends StatefulWidget {
@@ -19,26 +20,191 @@ class _ListaProdutoServicotState extends State<ListaProdutoServico> {
     super.initState();
   }
 
-  @override
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: AppBar(
-          title: Text('Serviço'),
+          title: Text('Produtos e Serviços'),
         ),
         body: ListView.builder(
+          padding: EdgeInsets.only(top: 20),
           itemCount: listadeProdutoServico.length,
           itemBuilder: (context, index) {
-            return ListTile(
-              subtitle: Text(listadeProdutoServico[index].valor),
-              title: Text(listadeProdutoServico[index].nome),
-            );
+            return _buildItem(context, listadeProdutoServico[index]);
           },
         ),
         floatingActionButton: FloatingActionButton.extended(
           foregroundColor: Colors.white,
           onPressed: () => Navigator.pushNamed(context, '/newProductService'),
           icon: Icon(Icons.add),
-          label: Text('Incluir Produto/Servico'),
+          label: Text('Produto/Serviço'),
         ));
+  }
+
+  _buildItem(BuildContext context, ProdutoServico produtoServico) {
+    return Container(
+      padding: EdgeInsets.only(left: 15, right: 15, bottom: 15),
+      child: Card(
+        elevation: 4,
+        child: Material(
+          child: InkWell(
+            onTap: () {
+              setState(() {
+                showSimpleCustomDialog(context, produtoServico);
+              });
+            },
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    produtoServico.nome,
+                    style: TextStyle(
+                      fontFamily: "Myriad Pro",
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: -.5,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Row(
+                        children: [
+                          Container(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  "Valor R\$: ",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                Text(
+                                  produtoServico.valor,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 8),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void showSimpleCustomDialog(
+      BuildContext context, ProdutoServico produtoServico) {
+    TextEditingController nome =
+        TextEditingController(text: produtoServico.nome);
+    TextEditingController valor =
+        TextEditingController(text: produtoServico.valor);
+    AlertDialog simpleDialog = AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      content: Container(
+        padding: EdgeInsets.all(5),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                controller: nome,
+                decoration: InputDecoration(
+                  isDense: false,
+                  labelText: "Nome:",
+                ),
+                onChanged: (v) {},
+              ),
+              SizedBox(height: 5),
+              TextFormField(
+                controller: valor,
+                decoration: InputDecoration(
+                  isDense: false,
+                  labelText: "Valor R\$: ",
+                ),
+                onChanged: (v) {},
+              ),
+              SizedBox(height: 5),
+            ],
+          ),
+        ),
+      ),
+      actions: <Widget>[
+        Row(
+          children: <Widget>[
+            RaisedButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(5),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text(
+                'CANCELAR',
+                style: TextStyle(fontSize: 12),
+              ),
+            ),
+            SizedBox(width: 5),
+            FlatButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(5),
+              ),
+              color: Colors.green,
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    'SALVAR',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                ],
+              ),
+              onPressed: () {
+                Map<String, dynamic> updateProdutoServico = {
+                  'nome': nome.text,
+                  'id': produtoServico.id,
+                  'valor': valor.text,
+                };
+                _updateProdutoServico(updateProdutoServico);
+                setState(() {
+                  Navigator.of(context).pop();
+                });
+                //Navigator.pushNamed(context, '/listaClientes');
+              },
+            ),
+            SizedBox(
+              width: 20,
+            ),
+          ],
+        ),
+      ],
+    );
+    showDialog(
+        context: context, builder: (BuildContext context) => simpleDialog);
+  }
+
+  void _updateProdutoServico(updateProdutoServico) {
+    // print(produtoServico.nome);
+    var produtoServico = new ProdutoServico(
+        nome: updateProdutoServico['nome'],
+        valor: updateProdutoServico['valor'],
+        id: updateProdutoServico['id']);
+    bacon.insertUpdate(objeto: produtoServico, tabela: 'ProdutoServico');
   }
 }
