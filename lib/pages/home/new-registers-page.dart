1655d27f067da7a53be22a3cea37f85831e8cb91
Hugo Alves <guinhoalves.cba@gmail.com>
2021-03-10 17:52:20 -0400

    Melhorias na listagem de Cadastros.

--- lib/pages/home/new-registers-page.dart
+++ lib/pages/home/new-registers-page.dart
@@ -10,36 +10,30 @@ class NewRegisters extends StatefulWidget {
 class _NewRegisterPageState extends State<NewRegisters> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: primaryLight,
-        height: MediaQuery.of(context).size.height * 0.90,
-        child: ListView(
-          padding: EdgeInsets.only(top: 20, left: 15, right: 15),
-          children: [
-            FlatButton.icon(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(25),
-              ),
-              icon: Icon(Icons.person_add, color: Colors.white),
-              color: secondary,
-              onPressed: () => Navigator.pushNamed(context, '/listaClientes'),
-              label: Text(
-                'Clientes',
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            FlatButton.icon(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(25),
-              ),
-              icon: Icon(Icons.add_box_outlined, color: Colors.white),
-              color: secondary,
-              onPressed: () =>
-                  Navigator.pushNamed(context, '/listaProdutoServico'),
-              label: Text('Produto/Serviço',
-                  style: TextStyle(color: Colors.white)),
-            )
-          ],
-        ));
+    return GridView.count(
+      crossAxisCount: 1,
+      children: List.generate(20, (index) {
+        return Container(
+          padding: EdgeInsets.all(15),
+          child: _buildCards(),
+        );
+      }),
+    );
+  }
+
+  Widget _buildCards() {
+    return Card(
+      child: InkWell(
+        splashColor: Colors.blue.withAlpha(30),
+        onTap: () {
+          print('Card Cadastro de Cliente');
+        },
+        child: Container(
+          width: double.infinity,
+          height: 100,
+          child: Text('A card that can be tapped'),
+        ),
+      ),
+    );
   }
 }
