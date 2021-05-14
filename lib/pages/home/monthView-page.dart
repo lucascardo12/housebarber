50fc63d9f802db2a41d42b6f1141c041c5dfd6b5
lucas <lucas.cardo123@hotmail.com>
2021-05-13 20:15:25 -0400

    add OBX na view Month

--- lib/pages/home/monthView-page.dart
+++ lib/pages/home/monthView-page.dart
@@ -4,7 +4,6 @@ import 'package:housebarber/controller/monthView-controller.dart';
 import 'package:housebarber/services/global.dart';
 import 'package:housebarber/model/agendamento.dart';
 import 'package:housebarber/pages/cadastros/addEvento-page.dart';
-import 'package:intl/intl.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class MonthViewPage extends GetView {
@@ -38,73 +37,75 @@ class MonthViewPage extends GetView {
               color: Colors.blue,
               iconSize: 38,
             )),
-        body: SfCalendar(
-            controller: controller.controller,
-            onTap: (CalendarTapDetails details) async {
-              if (details.appointments != null &&
-                  details.targetElement.toString() == 'CalendarElement.appointment') {
-                Get.bottomSheet(AddEventoPage(
-                  agenda: details.appointments.first,
-                ));
-              }
-            },
-            monthViewSettings: MonthViewSettings(
-              showAgenda: true,
-              monthCellStyle: MonthCellStyle(
-                textStyle: TextStyle(
-                  color: Colors.white,
-                  fontSize: 14,
-                ),
-              ),
-            ),
-            dataSource: AgendamentoDataSource(gb.listAgenda),
-            showNavigationArrow: true,
-            headerStyle: CalendarHeaderStyle(
-              textStyle: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-              ),
-            ),
-            backgroundColor: gb.primaryLight,
-            timeSlotViewSettings: TimeSlotViewSettings(
-              timeFormat: 'H',
-              timeTextStyle: TextStyle(
-                color: gb.secondary,
-                fontSize: 14,
-              ),
-            ),
-            view: CalendarView.month,
-            cellBorderColor: gb.secondary,
-            viewHeaderStyle: ViewHeaderStyle(
-              dayTextStyle: TextStyle(
-                color: Colors.white,
-                fontSize: 12,
-              ),
-            ),
-            appointmentBuilder: (
-              BuildContext context,
-              CalendarAppointmentDetails details,
-            ) {
-              final Agendamento agenda = details.appointments.first;
-              return Container(
-                  width: Get.width,
-                  height: Get.height,
-                  decoration: BoxDecoration(
-                    color: gb.secondary,
-                  ),
-                  child: Center(
-                      child: Text(
-                    controller.retornaTitulo(agenda),
-                    style: TextStyle(
+        body: Obx(() => gb.listAgenda.length > -1
+            ? SfCalendar(
+                controller: controller.controller,
+                onTap: (CalendarTapDetails details) async {
+                  if (details.appointments != null &&
+                      details.targetElement.toString() == 'CalendarElement.appointment') {
+                    Get.bottomSheet(AddEventoPage(
+                      agenda: details.appointments.first,
+                    ));
+                  }
+                },
+                monthViewSettings: MonthViewSettings(
+                  showAgenda: true,
+                  monthCellStyle: MonthCellStyle(
+                    textStyle: TextStyle(
                       color: Colors.white,
                       fontSize: 14,
-                      fontWeight: FontWeight.w500,
                     ),
-                    maxLines: 3,
-                    softWrap: false,
-                    overflow: TextOverflow.ellipsis,
-                    textAlign: TextAlign.center,
-                  )));
-            }));
+                  ),
+                ),
+                dataSource: AgendamentoDataSource(gb.listAgenda),
+                showNavigationArrow: true,
+                headerStyle: CalendarHeaderStyle(
+                  textStyle: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+                backgroundColor: gb.primaryLight,
+                timeSlotViewSettings: TimeSlotViewSettings(
+                  timeFormat: 'H',
+                  timeTextStyle: TextStyle(
+                    color: gb.secondary,
+                    fontSize: 14,
+                  ),
+                ),
+                view: CalendarView.month,
+                cellBorderColor: gb.secondary,
+                viewHeaderStyle: ViewHeaderStyle(
+                  dayTextStyle: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                  ),
+                ),
+                appointmentBuilder: (
+                  BuildContext context,
+                  CalendarAppointmentDetails details,
+                ) {
+                  final Agendamento agenda = details.appointments.first;
+                  return Container(
+                      width: Get.width,
+                      height: Get.height,
+                      decoration: BoxDecoration(
+                        color: gb.secondary,
+                      ),
+                      child: Center(
+                          child: Text(
+                        controller.retornaTitulo(agenda),
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                        ),
+                        maxLines: 3,
+                        softWrap: false,
+                        overflow: TextOverflow.ellipsis,
+                        textAlign: TextAlign.center,
+                      )));
+                })
+            : Container()));
   }
 }
